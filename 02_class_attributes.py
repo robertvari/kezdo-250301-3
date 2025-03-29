808d90f68a5b58b31af9a92fc1d0ee8e95c05bdd
Robert Vari <mail.robertvari@gmail.com>
2025-03-29 14:26:28 +0100

    update

---
+++ 02_class_attributes.py
@@ -0,0 +1,14 @@
+class Dice:
+    # attributes of the Dice class
+    sides = 6
+    color = "White"
+
+
+print(Dice.color)
+print(Dice.sides)
+
+Dice.sides = 10
+Dice.color = "Blue"
+
+print(Dice.color)
+print(Dice.sides)
