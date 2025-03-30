281c7cc76d3a375ac1352f56ace92c60760d1f66
Robert Vari <mail.robertvari@gmail.com>
2025-03-30 15:25:10 +0200

    update

---
+++ Blackjack/Blackjack.py
@@ -0,0 +1,23 @@
+import os
+
+class Blackjack:
+    def __init__(self):
+        self.clear_screen()
+        self.intro()
+
+    @staticmethod
+    def clear_screen():
+        if os.name == "nt":
+            os.system("cls")
+        else:
+            os.system("clear")
+
+    @staticmethod
+    def intro():
+        print("-"*50, "BLACKJACK", "-"*50)
+        print("Wellcome to Blackjack!")
+        print("Get as close to 21 as possible without going over. Beat the others.")
+        print("If your hand is higher than the others without going over 21 → You win.")
+        input("Press Enter to continue...")
+
+Blackjack()
