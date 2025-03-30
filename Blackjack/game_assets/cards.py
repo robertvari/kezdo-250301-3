aaac151da8198de34ae2be963d61d5feb891470e
Robert Vari <mail.robertvari@gmail.com>
2025-03-30 15:35:00 +0200

    update

---
+++ Blackjack/game_assets/cards.py
@@ -0,0 +1,18 @@
+class Card:
+    def __init__(self, name, value):
+        self.__name = name
+        self.__value = value
+
+    def __str__(self):
+        return f"Name: {self.__name} Value: {self.__value}"
+
+    def __repr__(self):
+        return self.__name
+
+if __name__ == "__main__":
+    card1 = Card("Club Ace", 11)
+    card2 = Card("Diamon King", 10)
+    card3 = Card("Spade 3", 3)
+    
+    deck = [card1, card2, card3]
+    print(deck)
