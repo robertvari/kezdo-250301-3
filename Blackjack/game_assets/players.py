7bd8cdcabd4b5c56efe31a0fddc73c06c6d3630d
Robert Vari <mail.robertvari@gmail.com>
2025-03-30 17:18:40 +0200

    update

--- Blackjack/game_assets/players.py
+++ Blackjack/game_assets/players.py
@@ -1,4 +1,4 @@
-import random
+import random, time
 
 class Player_BASE:
     def __init__(self):
@@ -11,12 +11,33 @@ class Player_BASE:
         self._name = self.get_random_name()
         self.__credits = random.randint(10, 100)
     
+    def draw(self, deck):
+        while self.playing:
+            if self.hand_value <= 19:
+                print(f"{self.name} draws card...")
+                time.sleep(2)
+                new_card = deck.draw()
+                if self.hand_value > 10 and new_card.value == 11:
+                    new_card.change_calue()
+
+                self.__hand.append(new_card)
+            else:
+                print(f"{self.name} finishes drawing")
+                self.playing = False
+
+    def show_hand(self):
+        print(self.__hand, f"Hand value: {self.hand_value}")
+
     @staticmethod
     def get_random_name():
         first_names = ["Liam", "Emma", "Noah", "Olivia", "Ethan", "Ava", "James", "Sophia", "Benjamin", "Mia"]
         last_names = ["Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez"]
 
         return f"{random.choice(first_names)} {random.choice(last_names)}"
+
+    @property
+    def hand_value(self):
+        return sum([card.value for card in self.__hand])
 
     @property
     def name(self):
@@ -52,12 +73,11 @@ class Computer(Player_BASE):
 
 
 if __name__ == "__main__":
-    player = Player()
+    from cards import Deck
+
+    deck = Deck()
+
     computer = Computer()
-
-    player.create()
     computer.create()
-
-    print(player.playing)
-    player.playing = False
-    print(player.playing)
+    computer.draw(deck)
+    computer.show_hand()
